2c5e7c450dd1182d8d7b70e76e56ea00fb15b3ed
Mnesikos <mnesikos@gmail.com>
2024-12-28 14:23:45 -0500

    add 11 new colors

--- src/main/java/com/github/mnesikos/flowerary/item/FloweraryColor.java
+++ src/main/java/com/github/mnesikos/flowerary/item/FloweraryColor.java
@@ -3,16 +3,27 @@ package com.github.mnesikos.flowerary.item;
 import net.minecraft.util.IStringSerializable;
 
 public enum FloweraryColor implements IStringSerializable {
+    BURNING("burning"),
     RED("red"),
     ORANGE("orange"),
     YELLOW("yellow"),
+    LIME("lime"),
+    MINT("mint"),
+    FREEZING("freezing"),
     CYAN("cyan"),
     BLUE("blue"),
+    LIGHT_BLUE("light_blue"),
+    LAVENDER("lavender"),
+    PURPLE("purple"),
     LILAC("lilac"),
     MAGENTA("magenta"),
     PINK("pink"),
+    PEACH("peach"),
+    MULTICOLOR("multicolor"),
     WHITE("white"),
-    BLACK("black");
+    PLATINUM("platinum"),
+    BLACK("black"),
+    WILTED("wilted");
 
     private final String name;
 

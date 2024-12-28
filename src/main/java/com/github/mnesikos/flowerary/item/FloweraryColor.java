17dba3f64bf99859b680255c2548550e1fee72bf
Mnesikos <mnesikos@gmail.com>
2024-12-28 14:33:01 -0500

    Update FloweraryColor.java
    assign associated vanilla dye colors

--- src/main/java/com/github/mnesikos/flowerary/item/FloweraryColor.java
+++ src/main/java/com/github/mnesikos/flowerary/item/FloweraryColor.java
@@ -1,42 +1,45 @@
 package com.github.mnesikos.flowerary.item;
 
+import net.minecraft.item.DyeColor;
 import net.minecraft.util.IStringSerializable;
 
 public enum FloweraryColor implements IStringSerializable {
-    BURNING("burning"),
-    RED("red"),
-    ORANGE("orange"),
-    YELLOW("yellow"),
-    LIME("lime"),
-    MINT("mint"),
-    FREEZING("freezing"),
-    CYAN("cyan"),
-    BLUE("blue"),
-    LIGHT_BLUE("light_blue"),
-    LAVENDER("lavender"),
-    PURPLE("purple"),
-    LILAC("lilac"),
-    MAGENTA("magenta"),
-    PINK("pink"),
-    PEACH("peach"),
-    MULTICOLOR("multicolor"),
-    WHITE("white"),
-    PLATINUM("platinum"),
-    BLACK("black"),
-    WILTED("wilted");
+    BURNING("burning", DyeColor.RED),
+    RED("red", DyeColor.RED),
+    ORANGE("orange", DyeColor.ORANGE),
+    YELLOW("yellow", DyeColor.YELLOW),
+    LIME("lime", DyeColor.LIME),
+    MINT("mint", DyeColor.LIME),
+    CYAN("cyan", DyeColor.CYAN),
+    FREEZING("freezing", DyeColor.LIGHT_BLUE),
+    LIGHT_BLUE("light_blue", DyeColor.LIGHT_BLUE),
+    BLUE("blue", DyeColor.BLUE),
+    LAVENDER("lavender", DyeColor.PURPLE),
+    PURPLE("purple", DyeColor.PURPLE),
+    LILAC("lilac", DyeColor.PURPLE),
+    MAGENTA("magenta", DyeColor.MAGENTA),
+    PINK("pink", DyeColor.PINK),
+    PEACH("peach", DyeColor.PINK),
+    MULTICOLOR("multicolor", DyeColor.WHITE),
+    WHITE("white", DyeColor.WHITE),
+    PLATINUM("platinum", DyeColor.LIGHT_GRAY),
+    BLACK("black", DyeColor.BLACK),
+    WILTED("wilted", DyeColor.BROWN);
 
     private final String name;
+    private final DyeColor dyeColor;
 
-    FloweraryColor(String name) {
+    FloweraryColor(String name, DyeColor dyeColor) {
         this.name = name;
-    }
-
-    public String toString() {
-        return this.name;
+        this.dyeColor = dyeColor;
     }
 
     @Override
     public String getSerializedName() {
         return this.name;
+    }
+
+    public DyeColor getDyeColor() {
+        return dyeColor;
     }
 }
